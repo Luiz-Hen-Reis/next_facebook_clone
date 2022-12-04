e73f3984e74446040cccc4905bc32b6cb5034ed2
Luiz-Hen-Reis <lhcontato2020@gmail.com>
2022-12-04 12:42:39 -0300

    adding the NEXTAUTH_SECRET

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth, { NextAuthOptions } from 'next-auth';
 import FacebookProvider from 'next-auth/providers/facebook';
 
 const authOptions: NextAuthOptions = {
+    secret:process.env.NEXTAUTH_SECRET,
     providers: [
         FacebookProvider({
             clientId: process.env.FACEBOOK_CLIENT_ID as string,
